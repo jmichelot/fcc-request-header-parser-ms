799aa2b009193b90b981c2533e43ff0f63a5cd34
Julien Michelot <jmichelot@yahoo.com>
2016-07-09 09:40:07 +0200

    v 1.0.0

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+// {"ipaddress":"80.12.33.34","language":"fr-FR","software":"Macintosh; Intel Mac OS X 10_11_5"}
+
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  var ip = req.headers.host;
+  var language = req.headers['accept-language']; 
+  var software = req.headers['user-agent'];
+  ip = ip.substr(0, ip.indexOf(":"));
+  software = software.match( /\((.*?)\)/ )[1];
+  res.json({'ipaddress': ip, 'language': language, 'software': software});
 });
 
 app.listen(port, function () {
